94b2eb06e4557d680125ab9d406533c17a171e43
ali mirzakhani <mirzakhani94@gmail.com>
2024-02-21 17:21:21 +0330

    rim

--- s3/mareis.cpp
+++ s3/mareis.cpp
@@ -11,25 +11,42 @@ template<typename t>
 class matris
 {
 private:
-    int* adad;
+    t** adad;
     int tool;
     int arz;
 
 public:
+    // void matris_saz(t* a)
+    // {
+    //     int i=0;
+    //     while(i<tool*arz)
+    //     {
+    //         adad[i]=a[i];
+    //         i++;
+    //     }
+    // }
     void matris_saz(t* a)
+{
+    adad =(int **) malloc(sizeof(t)*tool) ;
+    int k=0;
+    while()
+    int i=0, j=0; 
+    while(i<arz)
     {
-        int i=0;
-        while(i<tool*arz)
+        while(j<tool)
         {
-            adad[i]=a[i];
-            i++;
+            adad[i][j]=a[i*(tool)+j];
+            j++;
         }
+        j=0;
+        i++;
     }
+}
     matris(t* dade , int tool_dade,int arz_dade)
     {
         tool=tool_dade;
         arz=arz_dade;  
-        adad= new t[tool*arz];
+        // adad= new t[tool*arz];
         // int i=0;
         // while(i<arz)
         // {
@@ -39,88 +56,88 @@ public:
         matris_saz(dade);
     }
     
-    matris(const matris & dade)
-    {
-        tool=dade.tool;
-        arz=dade.arz;
-        // adad=(t*)malloc(sizeof(t*)*tool)
-        adad=new t[tool*arz];
-        int i=0;
-        // while(i<arz)
-        // {
-        //     adad[i]=(t)malloc(sizeof(t)*arz);
-        //     i++;
-        // }
-        matris_saz(dade.mbede());
-    }
-    t* mbede()
-    {
-        // t* r=(t*)malloc(sizeof(t*)*tool);
-        // int k=0;
-        // while(k<arz)
-        // {
-        //     adad[k]=(t)malloc(sizeof(t)*arz);
-        //     k++;
-        // }
-        t* r = new t[tool*arz];
-        int i=0 ; 
-        while (i<tool*arz)
-        {
-            r[i]=adad[i];    
-            i++;
-        }
-        return r;
-    }
-    t at(int makan_tool , int makan_arz)
-    {
-        return adad[makan_tool-1+(makan_arz-1)*tool]
-    }
-    t jam(const matris& a, int makan_tool,int makan_arz)
-    { 
-        t r = a.at(makan_tool,makan_arz)+adad[makan_tool][makan_arz];
-        return r;
-    }
-    t kam(const matris & a , int makan_tool , int makan_arz)
-    { 
-        t r = adad[makan_tool][makan_arz] -a[makan_tool][makan_arz];
-        return r;
-    }
-    t* jam(const matris& a)
-    {
-        if(a.tool==tool)
-        {
-            t* r=(t*)malloc(sizeof(t*)*tool);
-            int i =0, j=0 ; 
-            while(i<tool)
-            {
-                while (j<arz)
-                {
-                r[i][j]=jam(a,i,j);
-                j++;
-                }
-                j=0;
-                i++;
-            }
-            return r;
-        }
-        return nullptr;
-    }
-    t* kam(const matris& a)
-    {
-            t* r=(t*)malloc(sizeof(t*)*tool);
-            int i =0, j=0 ; 
-            while(i<tool)
-            {
-                while (j<arz)
-                {
-                r[i][j]=kam(a,i,j);
-                j++;
-                }
-                j=0;
-                i++;
-            }
-            return r;
-    }
+    // matris(const matris & dade)
+    // {
+    //     tool=dade.tool;
+    //     arz=dade.arz;
+    //     // adad=(t*)malloc(sizeof(t*)*tool)
+    //     adad=new t[tool*arz];
+    //     int i=0;
+    //     // while(i<arz)
+    //     // {
+    //     //     adad[i]=(t)malloc(sizeof(t)*arz);
+    //     //     i++;
+    //     // }
+    //     matris_saz(dade.mbede());
+    // }
+    // t* mbede()
+    // {
+    //     // t* r=(t*)malloc(sizeof(t*)*tool);
+    //     // int k=0;
+    //     // while(k<arz)
+    //     // {
+    //     //     adad[k]=(t)malloc(sizeof(t)*arz);
+    //     //     k++;
+    //     // }
+    //     t* r = new t[tool*arz];
+    //     int i=0 ; 
+    //     while (i<tool*arz)
+    //     {
+    //         r[i]=adad[i];    
+    //         i++;
+    //     }
+    //     return r;
+    // }
+    // t at(int makan_tool , int makan_arz)
+    // {
+    //     return adad[makan_tool-1+(makan_arz-1)*tool]
+    // }
+    // t jam(const matris& a, int makan_tool,int makan_arz)
+    // { 
+    //     t r = a.at(makan_tool,makan_arz)+at(makan_tool,makan_arz);
+    //     return r;
+    // }
+    // t kam(const matris & a , int makan_tool , int makan_arz)
+    // { 
+    //     t r = at(makan_tool,makan_arz) -a.at(makan_tool,makan_arz);
+    //     return r;
+    // }
+    // t* jam(const matris& a)
+    // {
+    //     if(a.tool==tool)
+    //     {
+    //         t* r=(t*)malloc(sizeof(t*)*tool);
+    //         int i =0, j=0 ; 
+    //         while(i<tool)
+    //         {
+    //             while (j<arz)
+    //             {
+    //             r[i][j]=jam(a,i,j);
+    //             j++;
+    //             }
+    //             j=0;
+    //             i++;
+    //         }
+    //         return r;
+    //     }
+    //     return nullptr;
+    // }
+    // t* kam(const matris& a)
+    // {
+    //         t* r=(t*)malloc(sizeof(t*)*tool);
+    //         int i =0, j=0 ; 
+    //         while(i<tool)
+    //         {
+    //             while (j<arz)
+    //             {
+    //             r[i][j]=kam(a,i,j);
+    //             j++;
+    //             }
+    //             j=0;
+    //             i++;
+    //         }
+    //         return r;
+    // }
     // t* zarb()
 };
 
@@ -128,6 +145,5 @@ int main()
 {
     int r[2*2]={2,3,1,2};
     matris<int> m(r,2,2);
-
     return 0;
 }
---
+++ s4/c4.cpp
@@ -0,0 +1,13 @@
+
+class st
+{
+private:
+    char* s;
+    int len;
+public:
+    st(char* q, int to)
+    {
+
+    }
+};
+
