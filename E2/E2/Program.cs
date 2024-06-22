397761abeea1a9b7c018270caa53515e98dd41e4
ali mirzakhani <mirzakhani94@gmail.com>
2024-06-22 10:23:29 +0330

    ghabl azmoon

---
+++ E2/E2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+public class program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
